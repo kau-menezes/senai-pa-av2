4d288281cc59389749358fc08450c86643e06f5a
kau-menezes <kau.vmenezes@gmail.com>
2024-10-09 16:27:39 -0300

    responsividade e adaptabilidade da header (usada pelas três páginas!!

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,10 +10,10 @@ interface IHeader {
 const Header = ( { route1, route2, route3 }: IHeader ) => {
 
     return (
-        <nav className="font-semibold flex flex-row gap-x-5 bg-amber-800 justify-center p-3 font-karlaFont h-[6vh]">
-          <Link href={ROUTES.firstRoute} className="font-semibold hover:cursor-pointer hover:bg-amber-600 text-center min-w-24 py-1 px-2 text-white bg-amber-700">{route1}</Link>
-          <Link href={ROUTES.secondRoute} className="font-semibold hover:cursor-pointer hover:bg-amber-600 text-center min-w-24 py-1 px-2 text-white bg-amber-700">{route2}</Link>
-          <Link href={ROUTES.thirdRoute} className="font-semibold hover:cursor-pointer hover:bg-amber-600 text-center min-w-24 py-1 px-2 text-white bg-amber-700">{route3}</Link>
+        <nav className="font-semibold flex flex-row flex-wrap gap-5 bg-amber-800 justify-center p-3 font-karlaFont min-h-[6vh] h-fit">
+          <Link href={ROUTES.firstRoute} className=" text-wrap font-semibold hover:cursor-pointer hover:bg-amber-600 text-center min-w-24 py-1 px-2 text-white bg-amber-700">{route1}</Link>
+          <Link href={ROUTES.secondRoute} className=" text-wrap font-semibold hover:cursor-pointer hover:bg-amber-600 text-center min-w-24 py-1 px-2 text-white bg-amber-700">{route2}</Link>
+          <Link href={ROUTES.thirdRoute} className=" text-wrap font-semibold hover:cursor-pointer hover:bg-amber-600 text-center min-w-24 py-1 px-2 text-white bg-amber-700">{route3}</Link>
           
         </nav>
     )
